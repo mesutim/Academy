306a1e9e7481c29fb35831c7cc271d126b6b0a6f
Mesud <masoud.aziznejad@gmail.com>
2023-11-17 13:38:45 +0330

    Create Admin Area, Add AdminLayout and AdminPanel HomeController and Index View

--- Academy.Web/Program.cs
+++ Academy.Web/Program.cs
@@ -32,6 +32,9 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
